f10c42ed713c9ad1e0e3fe3dc4b9bd133b8e541f
SaitoDai <dai2525yuu@icloud.com>
2025-07-06 15:46:25 +0900

    全ファイルを追加

--- main.py
+++ main.py
@@ -11,6 +11,19 @@ pygame.display.set_caption("RhythmGame")
 clock = pygame.time.Clock()
 FPS = 60
 
+#レーンの描画
+LANE_HEIGHT = 80
+LANE_GAP = 20
+NUM_LANES = 4
+LANE_Y = [100 + i*(LANE_HEIGHT + LANE_GAP) for i in range(NUM_LANES)]
+HIT_LINE_X = 150
+LANE_END_X = SCREEN_WIDTH -200
+
+def draw_lanes():
+    for y in LANE_Y:
+        pygame.draw.rect(screen,(50,50,50),(HIT_LINE_X, y, LANE_END_X - HIT_LINE_X, LANE_HEIGHT))
+    pygame.draw.line(screen, (255,255,255), (HIT_LINE_X, 0), (HIT_LINE_X,SCREEN_HEIGHT), 2)
+
 def main():
     running = True
     while running:
@@ -18,7 +31,8 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
         
-        screen.fill((30,30,30))
+        screen.fill((0,0,0))
+        draw_lanes()
         
         pygame.display.flip()
         
